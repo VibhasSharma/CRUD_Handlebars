519bf54a5f06f354d6fc506bb0a533467cfc91cc
Vibhas Sharma <vibhascr734@gmail.com>
2020-07-14 11:10:53 +0530

    Added logic for Record UPDATE

--- Controller/crudContacts.js
+++ Controller/crudContacts.js
@@ -10,7 +10,11 @@ router.get('/', (req,res) => {
 });
 
 router.post('/', (req,res) => {   //route already provided in server.js
-    insertRecord(req,res);
+    if(req.body._id == ''){
+        insertRecord(req,res);
+    }else{
+        updateRecord(req, res)
+    }    
 });
 
 function insertRecord(req, res) {
@@ -40,6 +44,24 @@ function insertRecord(req, res) {
         }
     });
 };
+
+function updateRecord(req, res) {
+    Contact.findOneAndUpdate({_id: req.body._id}, req.body, {new: true}, (err, doc) => {           //With new: true, inside of the doc variable we'll have the 
+        if(!err){                                                                                  //value of updated fields, if false we'll have old value of record
+            res.redirect('contactsList/list');
+        }else{
+            if(err.name == 'ValidationError'){
+                handleValidationError(err, req.body);
+                res.render('contacts/addOrEdit', {
+                    viewTitle: 'Update Contact info',
+                    contact: req.body
+                })
+            }else{
+                console.log("Error during contact update" + err);
+            }
+        }                                                                                           
+    });
+}
 
 
 router.get('/list', (req, res) => {
@@ -80,7 +102,7 @@ router.get('/:id', (req, res) => {   //This is the mongo DB id inorder to retrie
         if(!err){
             res.render('contacts/addOrEdit', {
                 viewTitle: "Update Contact Information",
-                contactUpdate: doc.toJSON
+                contact: doc.toJSON()
             });
             console.log(doc);
         }else{
